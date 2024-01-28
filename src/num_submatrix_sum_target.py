61a274d1c06e909d69e81573f091ba249fd7d9cd
Sandro <31536101+sandromund@users.noreply.github.com>
2024-01-28 14:35:11 +0100

    1074. Number of Submatrices That Sum to Target

--- src/num_submatrix_sum_target.py
+++ src/num_submatrix_sum_target.py
@@ -11,8 +11,24 @@ different if they have some coordinate that is different: for example, if x1 != 
 https://leetcode.com/problems/number-of-submatrices-that-sum-to-target/description/?envType=daily-question&envId=2024-01-28
 
 """
-from typing import  List
+from typing import List
 
 
 def num_sub_matrix_sum_target(matrix: List[List[int]], target: int) -> int:
-    return 0
+    """
+    https://leetcode.com/problems/number-of-submatrices-that-sum-to-target/solutions/4636406/c-java-python-javascript-2-approaches-explained/?envType=daily-question&envId=2024-01-28
+    """
+    m, n = len(matrix), len(matrix[0])
+    for row in range(m):
+        for col in range(1, n):
+            matrix[row][col] += matrix[row][col - 1]
+    count = 0
+    for c1 in range(n):
+        for c2 in range(c1, n):
+            prefix_sum_count = {0: 1}
+            sum_val = 0
+            for row in range(m):
+                sum_val += matrix[row][c2] - (matrix[row][c1 - 1] if c1 > 0 else 0)
+                count += prefix_sum_count.get(sum_val - target, 0)
+                prefix_sum_count[sum_val] = prefix_sum_count.get(sum_val, 0) + 1
+    return count
